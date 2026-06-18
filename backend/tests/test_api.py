ed03242ce8371477cfb4031159a82c6d44701c77
volkovdmitri <75804299+volkovdmitri@users.noreply.github.com>
2026-06-18 14:14:40 +0000

    formatting

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -2,8 +2,9 @@ from fastapi.testclient import TestClient
 
 from app.application import create_app
 
-app = create_app()  
+app = create_app()
 client = TestClient(app)
+
 
 def test_root():
     response = client.get("/")
