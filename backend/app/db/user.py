6b257c310b573a1a87ec75e94a8b50c5c23ffbaa
volkovdmitri <75804299+volkovdmitri@users.noreply.github.com>
2026-06-22 11:52:40 +0000

    linting

--- backend/app/db/user.py
+++ backend/app/db/user.py
@@ -7,11 +7,7 @@ from app.db.base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(
-        Integer,
-        primary_key=True,
-        autoincrement=True
-    )
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
     created_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
